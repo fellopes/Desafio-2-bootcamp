0e6163d9a14e83f92120f78d8fb415a1d4a80796
fellopes <77412519+fellopes@users.noreply.github.com>
2021-01-24 16:14:13 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,53 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs } = request.body;
+  const repository = repositories.find(repository=> repository.id==id);
+  if(!repository){
+    return response.status(400).json();
+  }
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const index = repositories.findIndex(repository=> repository.id==id)
+  if(index==-1){
+    return response.status(400).json();
+  }
+  repositories.splice(index,1);
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repository = repositories.find(Element=> Element.id==id);
+  if(!repository){
+    return response.status(400).json();
+  }
+  repository.likes += 1;
+  return  response.json(repository);
 });
 
 module.exports = app;
